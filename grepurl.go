74b391bdea4ebb6c42986b183d8405f78f48145b
Dan O'Huiginn <daniel@ohuiginn.net>
2015-04-08 21:08:54 +0200

    Set intersection code
    [yes, this truly is me working from the basics out]

--- grepurl.go
+++ grepurl.go
@@ -2,7 +2,56 @@ package grepurl
 
 import(
 	"fmt"
+	"sort"
 )
+
+func build_sorted_array(unsorted []int) []int {
+	// this should also be removing duplicates?
+	sort.Ints(unsorted)
+	return unsorted
+}
+
+func intersect_two_slow(one []int, two []int) []int{
+	inters := []int {}
+	one = build_sorted_array(one)
+	two = build_sorted_array(two)
+	operations := 0
+	for i_one, _ := range one{
+		for i_two, _ := range two {
+			operations += 1
+			if(one[i_one] == two[i_two]){
+				inters = append(inters, one[i_one])
+			}
+		}
+	}
+	fmt.Println(operations, "operations")
+	return inters
+}
+
+
+func intersect_two(one []int, two []int) []int{
+	inters := []int {}
+	one = build_sorted_array(one)
+	two = build_sorted_array(two)
+	operations := 0
+	i_two := 0
+	for i_one, _ := range one{
+		for i_two < len(two){
+			operations += 1
+			if(two[i_two] == one[i_one]){
+				inters = append(inters, one[i_one])
+				break
+			}
+			if two[i_two] > one[i_one]{
+				break
+			}
+			i_two += 1
+		}
+	}
+	fmt.Println(operations, "operations")
+	return inters
+}
+
 
 func main(){
 	fmt.Println("Hello world")
--- grepurl_test.go
+++ grepurl_test.go
@@ -3,8 +3,36 @@ package grepurl
 import (
 	"testing"
 	"fmt"
+	"math/rand"
+	"reflect"
 )
 
-func TestGrep(t *testing.T){
-	fmt.Println("all ready")
+func TestBuildSorted(t *testing.T){
+	// for now, this is just testing the builtin sort function
+	// but it will need to work with memory-mapped sorted arrays	
+	var tar [30]int
+	testslice := tar[:]
+	for i,_ := range testslice {
+		testslice[i] = rand.Intn(9999)
+	}
+	inp := build_sorted_array(testslice)
+	for i := 0; i<len(inp)-1;i++ {
+		if(inp[i] > inp[i+1]){
+			t.Fail()	
+		}
+	}
+	fmt.Println(inp)
+}
+
+func TestSimpleIntersect(t *testing.T){
+	// find the intersection of two
+	one := []int{5,2,7,4,8,98,97,39,50,40}
+	two := []int{44,2,7,3,9}
+	expected_inters := []int{2,7}
+	inters := intersect_two(one, two)
+	fmt.Println(inters)
+	if(!reflect.DeepEqual(inters, expected_inters)){
+		t.Fail()
+	}
+	_ = inters
 }

5aea89175e3403e4e98a7cc978bcc014622c8a0b
Dan O'Huiginn <daniel@ohuiginn.net>
2016-01-13 17:50:13 +0100

    Working search for (three-char) substring of URLS
    - build a basic map data structure
    - secondary map to key URLs to integer identifiers

--- grepurl.go
+++ grepurl.go
@@ -1,10 +1,26 @@
 package grepurl
 
 import(
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"sort"
-)
+	)
 
+//Map id -> URL
+var urlcodes map[int]string
+var nexturlcode int
+
+// map TLA --> list of items
+var occurances = make(map[string][]int)
+
+
+func _init(){
+	//set up the global vars
+	urlcodes = make(map[int]string)
+	nexturlcode = 0
+}
 
 func build_sorted_array(unsorted []int) []int {
 	// this should also be removing duplicates?
@@ -38,6 +54,14 @@ func intersect_two_slow(one []int, two []int) []int{
 	return inters
 }
 
+func SplitNgram(url string) []string{
+	// Split an url into three-character chunks
+	results := []string{}
+	for i := 0; i < len(url) - 2; i++ {
+		results = append(results, url[i:i+3])
+	}
+	return results
+}
 
 func intersect_two(one []int, two []int) []int{
 	inters := []int {}
@@ -62,7 +86,41 @@ func intersect_two(one []int, two []int) []int{
 	return inters
 }
 
+func ngrams_from_file(filepath string){
+	_init()
+
+	file, err := os.Open(filepath)
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		line := scanner.Text()
+		urlcodes[nexturlcode] = line
+		for _, ng := range SplitNgram(line){
+			occurances[ng] = append(occurances[ng], nexturlcode)
+			fmt.Println(ng)
+			}
+		nexturlcode++
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func urlmatches(tla string) []string{
+	res := []string{}
+	for _, i := range occurances[tla]{
+		res = append(res, urlcodes[i])
+	}
+	return res
+}
+
 
 func main(){
 	fmt.Println("Hello world")
 }
+
+
--- grepurl_test.go
+++ grepurl_test.go
@@ -2,10 +2,21 @@ package grepurl
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
 	"fmt"
 	"math/rand"
 	"reflect"
+	"github.com/danohuiginn/btree"
 )
+
+func cmp(a, b int) int {
+	return a - b
+}
+
+func TestBTree(t *testing.T){
+	_ = btree.TreeNew(cmp)
+} 
+
 
 func TestBuildSorted(t *testing.T){
 	// for now, this is just testing the builtin sort function
@@ -45,11 +56,12 @@ func randomInts(length int) []int{
 	return output[:]
 }
 
+
 func TestIntersect(t *testing.T){
 	// try intersections with long lists of random ints
 	// it takes ~6.5 seconds to handle intersection of 2
 	// slices of 10 million integers each
-	lengths := [] int {1,100,1000,10000,100000,10000000}
+	lengths := [] int {1,100,1000,10000,100000}
 	for _, len_one := range lengths {
 		fmt.Print("building random ints: one...")
 		fmt.Println("done")
@@ -62,4 +74,22 @@ func TestIntersect(t *testing.T){
 			_ = intersect_two(one,two)
 		}
 	}
+}
+
+
+func TestSplitNgram(t *testing.T){
+	// see what happens when we split an url
+	url := "example.com"
+	expected := []string{"exa", "xam", "amp", "mpl", "ple", "le.", "e.c", ".co", "com"}
+	result := SplitNgram(url)
+	for i, exp := range expected {
+		assert.Equal(t, result[i], exp, "ngram splitting non-match")
+	}
+}
+
+func TestNgramsFromFile(t *testing.T){
+	ngrams_from_file("/home/src/golang/src/github.com/danohuiginn/grepurl/testdata.txt")
+	res := urlmatches("tiv")
+	assert.Equal(t, res,  []string{"http://exple.tive.org/blarg/2015/09/20/bourne-aesthetic",
+		"http://kamiel.creativechoice.org/2015/09/10/will-work-for-the-commons/"})
 }
